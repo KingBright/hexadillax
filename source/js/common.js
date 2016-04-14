713d863c98e308efdaf41d2f04d27a682db1c9d1
KingBright <jl.kingbright@gmail.com>
2016-04-14 23:04:33 +0800

    If already has the image url for today, then just use it without making another request.

--- source/js/common.js
+++ source/js/common.js
@@ -1,22 +1,68 @@
-var cors = function (target, callback) {
-    $.getJSON("http://query.yahooapis.com/v1/public/yql?" +
-        "q=select * from html where url='" +
-        encodeURIComponent(target) +
-        "'&callback=?", function (response) {
-        var data = response;
-        if (data.results) {
-            data = data.results[0];
-            if (data) {
-                data = data.replace(/<\/?[^>]*>/g, '');
-                data = data.replace(/[ | ]*\n/g, '\n');
-                data = data.replace(/&nbsp;/ig, '');
+function dateFormat(date, format) {
+    if (format === undefined) {
+        format = date;
+        date = new Date();
+    }
+    var map = {
+        "M": date.getMonth() + 1, //月份
+        "d": date.getDate(), //日
+        "h": date.getHours(), //小时
+        "m": date.getMinutes(), //分
+        "s": date.getSeconds(), //秒
+        "q": Math.floor((date.getMonth() + 3) / 3), //季度
+        "S": date.getMilliseconds() //毫秒
+    };
+    format = format.replace(/([yMdhmsqS])+/g, function (all, t) {
+        var v = map[t];
+        if (v !== undefined) {
+            if (all.length > 1) {
+                v = '0' + v;
+                v = v.substr(v.length - 2);
+            }
+            return v;
+        }
+        else if (t === 'y') {
+            return (date.getFullYear() + '').substr(4 - all.length);
+        }
+        return all;
+    });
+    return format;
+}
+
+$(function () {
+    // Set background image, read from Bing.com and uses yql to make cors
+    (function (target, callback) {
+        var today = dateFormat(new Date(), 'yyyy-MM-dd');
+        today = today.replace(new RegExp('-', 'g'), '');
+
+        if (localStorage) {
+            var startDate = localStorage.getItem("start_time");
+            var endDate = localStorage.getItem("end_time");
+            if (startDate == today || endDate == today) {
+                var imageUrl = localStorage.getItem("image_url");
+                if (imageUrl) {
+                    $.backstretch(imageUrl);
+                    return;
+                }
             }
         }
-        callback && callback(data);
-    })
-}
-$(function () {
-    cors("https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1", function (data) {
+
+        $.getJSON("http://query.yahooapis.com/v1/public/yql?" +
+            "q=select * from html where url='" +
+            encodeURIComponent(target) +
+            "'&callback=?", function (response) {
+            var data = response;
+            if (data.results) {
+                data = data.results[0];
+                if (data) {
+                    data = data.replace(/<\/?[^>]*>/g, '');
+                    data = data.replace(/[ | ]*\n/g, '\n');
+                    data = data.replace(/&nbsp;/ig, '');
+                }
+            }
+            callback && callback(data);
+        })
+    })("https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1", function (data) {
         var json = JSON.parse(data);
         var images = json.images;
         if (!images || images.length == 0) {
@@ -26,22 +72,14 @@ $(function () {
 
         var image = images[0];
         var imageUrl = "https://www.bing.com" + image.url;
-        var time = image.fullstartdate;
+        var startTime = image.startdate;
+        var endTime = image.enddate;
 
         if (localStorage) {
-            var lastUpdateTime = localStorage.getItem("update_time");
-            var lastImageUrl = localStorage.getItem("image_url");
-            if (lastUpdateTime == time && lastImageUrl == imageUrl) {
-                $.backstretch(imageUrl);
-                return;
-            }
-
-            localStorage.setItem("update_time", time);
+            localStorage.setItem("start_time", startTime);
+            localStorage.setItem("end_time", endTime);
             localStorage.setItem("image_url", imageUrl);
         }
-
-        console.log(imageUrl)
-
         $.backstretch(imageUrl);
     });
 
